21d5d636f58d53f0d524c1ab0c5fef62b34899e4
mryash08 <yashchodvadiya203@gmail.com>
2024-10-15 17:59:03 +0530

    add Expanse layout

---
+++ app/src/main/java/com/example/khatabook/bill/Activity/activity_expense_detail.java
@@ -0,0 +1,29 @@
+package com.example.khatabook.bill.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.khatabook.R;
+import com.example.khatabook.databinding.ActivityExpenseDetailBinding;
+
+public class activity_expense_detail extends AppCompatActivity {
+
+    ActivityExpenseDetailBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityExpenseDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
